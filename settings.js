efa7bf3856458ba5576ba48a9774ebcc610f2bf2
Daniel0785 <159941606+Daniel0785@users.noreply.github.com>
2025-03-25 14:29:19 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "mganga nodp",
   botOwner: 'mganga nodp', // Your name
   ownerNumber: '254743244262', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
